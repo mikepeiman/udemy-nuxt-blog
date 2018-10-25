52521ad9644810c324b497fd362cff61b9643964
Mike Peiman <mikepeiman@gmail.com>
2018-10-25 12:32:05 -0700

    Refactored post preview into component with data props

---
+++ components/Posts/PostPreview.vue
@@ -0,0 +1,75 @@
+<template>
+<nuxt-link :to="'/posts/' + id" class="post-preview container">
+  <article>
+    <div class="post-thumbnail" :style="{backgroundImage: 'url(' + thumbnail + ')'}"></div>
+    <div class="post-content">
+      <h1 class="post-title">{{ title }}</h1>
+      <p class="post-snippet">{{ previewText }}</p>
+    </div>
+  </article>
+</nuxt-link>
+</template>
+
+<script>
+export default {
+  name: 'PostPreview',
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    previewText: {
+      type: String,
+      required: true
+    },
+    thumbnail: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.post-thumbnail {
+  width: 100%;
+  height: 200px;
+  background-repeat: no-repeat;
+  background-position: 0 0;
+  background-size: cover;
+  border:  none;
+}
+.post-preview {
+  display: flex;
+  flex-direction: column;
+  /* border: 1px solid white; */
+  padding: 0;
+}
+.post-snippet {
+  padding: 1em;
+  margin-bottom: 1em;
+}
+.post-content {
+  color: #222;
+}
+.post-title {
+    font-family: 'Montserrat', 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 3em;
+  border-bottom: 1px solid #eee;
+  /* margin-bottom: 1em; */
+  padding-bottom: 10px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,18 @@
 <template>
 <section class="home-page">
-  <section class="">
-    <section class="intro container">
+  <section class="intro">
+    <section class="container">
       <h1 class="title">Live a Life Examined</h1>
 
     </section>
     <section class="featured-posts">
-      <nuxt-link :to="'/posts/' + 1" class="post-preview container">
-        <article>
-          <div class="post-thumbnail" style="background-image: url('https://images.pexels.com/photos/953206/pexels-photo-953206.jpeg?auto=compress&cs=tinysrgb&h=350')"></div>
-          <div class="post-content">
-            <h1 class="post-title">Post title</h1>
-            <p class="post-snippet">Preview text</p>
-          </div>
-        </article>
-      </nuxt-link>
-      <nuxt-link :to="'/posts/' + 2" class="post-preview container">
-        <article class="post-preview">
-          <div class="post-thumbnail" style="background-image: url('https://images.pexels.com/photos/356808/pexels-photo-356808.jpeg?auto=compress&cs=tinysrgb&h=350')"></div>
-          <div class="post-content">
-            <h1 class="post-title">Post title</h1>
-            <p class="post-snippet">Preview text</p>
-          </div>
-        </article>
-      </nuxt-link>
+      <PostPreview 
+        id="1"
+        thumbnail="https://images.pexels.com/photos/953206/pexels-photo-953206.jpeg?auto=compress&cs=tinysrgb&h=350"
+        title="Hello there!"
+        previewText="This is my first post!"/>
+      <PostPreview />
+      <PostPreview />
     </section>
   </section>
 
@@ -31,19 +20,17 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
+import PostPreview from '@/components/Posts/PostPreview'
 export default {
   components: {
-    Logo
-  },
-  data() {
-    return {
-
-    }
+    PostPreview
   }
+},
+methods: {
+  
 }
 </script>
+
 
 <style scoped>
 .home-page {
@@ -64,13 +51,7 @@ export default {
   margin-bottom: 2em;
 }
 
-.post-thumbnail {
-  width: 100%;
-  height: 200px;
-  background-position: center;
-  background-size: cover;
-  border:  none;
-}
+
 .title {
   font-family: 'Montserrat', 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
@@ -82,29 +63,7 @@ export default {
   margin-bottom: 1em;
   /* letter-spacing: 1px; */
 }
-.post-preview {
-  display: flex;
-  flex-direction: column;
-  /* border: 1px solid white; */
-  padding: 0;
-}
-.post-snippet {
-  padding: 1em;
-  margin-bottom: 1em;
-}
-.post-content {
-  color: #222;
-}
-.post-title {
-    font-family: 'Montserrat', 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 3em;
-  border-bottom: 1px solid #eee;
-  /* margin-bottom: 1em; */
-  padding-bottom: 10px;
-}
+
 .subtitle {
   font-weight: 300;
   font-size: 42px;
