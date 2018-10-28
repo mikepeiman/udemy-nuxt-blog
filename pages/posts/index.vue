0c2aacf132afafa935210e3e47b89b5838cb68fb
Mike Peiman <mikepeiman@gmail.com>
2018-10-28 11:50:50 -0700

    refactored asyncData on /pages index to promise vs. callback

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -11,28 +11,55 @@ export default {
   components: {
     PostsList
   },
-  asyncData(context, callback) {
-    console.log('asyncData executed in /posts')
-    setTimeout(() => {
-      callback(null, {
-        loadedPosts: [
-          {
-            id: '1',
-            title: 'Post One',
-            previewText: 'This is the first post',
-            thumbnail: 'https://images.pexels.com/photos/953206/pexels-photo-953206.jpeg?auto=compress&cs=tinysrgb&h=350'
-          },
-          {
-            id: '2',
-            title: 'Post Two',
-            previewText: 'This is the second post',
-            thumbnail: 'https://images.pexels.com/photos/1124092/pexels-photo-1124092.jpeg?cs=srgb&dl=abstract-art-background-1124092.jpg&fm=jpg'
-          }
-        ]
-      })
-    }, 1500)
+  // data() {
+  //   return {
+  //     loadedPosts: [{
+  //         id: '1',
+  //         title: 'Post One',
+  //         previewText: 'This is the first post',
+  //         thumbnail: 'https://images.pexels.com/photos/953206/pexels-photo-953206.jpeg?auto=compress&cs=tinysrgb&h=350'
+  //       },
+  //       {
+  //         id: '2',
+  //         title: 'Post Two',
+  //         previewText: 'This is the second post',
+  //         thumbnail: 'https://images.pexels.com/photos/1124092/pexels-photo-1124092.jpeg?cs=srgb&dl=abstract-art-background-1124092.jpg&fm=jpg'
+  //       }
+  //     ]
+  //   }
+  // },
+ asyncData(context) {
+    return new Promise((resolve, reject) => {
+      // setTimeout(() => {
+       resolve({
+          loadedPosts: [
+            {
+              id: "1",
+              title: "First Post",
+              previewText: "This is our first post!",
+              thumbnail:
+                "https://images.pexels.com/photos/953206/pexels-photo-953206.jpeg?auto=compress&cs=tinysrgb&h=350"
+            },
+            {
+              id: "2",
+              title: "Second Post",
+              previewText: "This is our second post!",
+              thumbnail:
+                "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+            }
+          ]
+        });
+      // }, 1000);
+      // reject(new Error())
+    })
+    .then(data => {
+      return data
+    })
+    .catch(e => {
+      context.error(e);
+    });
   }
-}
+};
 </script>
 
 <style scoped>
